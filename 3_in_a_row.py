2add026736b99f89a1bf9b2467efb56dddc5125d
ElPayo <elpayov2@gmail.com>
2022-11-16 01:09:30 +0000

    Hecho

--- 3_in_a_row.py
+++ 3_in_a_row.py
@@ -1,22 +1,22 @@
-import os
 # Jugador 1: ❌
 # Jugador 2: 🟢
 match_field = []
 playing = True
 player1 = True
+moves = 0
 
 for i in range(3):
     match_field.append([])
     for j in range(3):
         match_field[i].append("🔳")
 
-while playing:
+while moves < 9:
     for row in match_field:
         for column in row:
             print(column, end=" ")
         print()
     if player1:
-        user_input = input("Jugador 1 introduce las coordenadas de la casilla separadas por comas: ").split(",")
+        user_input = input("Player 1 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
         row = int(user_input[0])
         column = int(user_input[1])
         if row and column <= 3 and match_field[row - 1][column - 1] == "🔳":
@@ -24,11 +24,11 @@ while playing:
             column -= 1
             match_field[row][column] = "❌"
             player1 = not player1
-            os.system("cls")
+            moves += 1
         else:
             print("Introduzca una coordenada valida")
     else:
-        user_input = input("Jugador 2 introduce las coordenadas de la casilla separadas por comas: ").split(",")
+        user_input = input("Player 2 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
         row = int(user_input[0])
         column = int(user_input[1])
         if row and column <= 3 and match_field[row - 1][column - 1] == "🔳":
@@ -36,6 +36,25 @@ while playing:
             column -= 1
             match_field[row][column] = "🟢"
             player1 = not player1
-            os.system("cls")
+            moves += 1
         else:
             print("Introduzca una coordenada valida")
+
+match match_field:
+    case match_field if match_field[0][0] == match_field[1][1] and match_field[1][1] == match_field[2][2]:
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[0][0] == match_field[1][0] and match_field[1][0] == match_field[2][0]:
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[0][0] == match_field[0][1] and match_field[0][1] == match_field[0][2]:
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case _:
+        print("No one is the winner")
