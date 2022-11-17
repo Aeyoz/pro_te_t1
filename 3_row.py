4c84ac4e0c9bdb023ab43df7a89a0b792216e072
ElPayo <elpayov2@gmail.com>
2022-11-17 23:47:56 +0000

    Programa al 90 % funcional

--- 3_row.py
+++ 3_row.py
@@ -1,23 +1,18 @@
 # Jugador 1: ❌
 # Jugador 2: 🟢
-match_field = []
+match_field = [["🔳","🔳","🔳"],["🔳","🔳","🔳"],["🔳","🔳","🔳"]]
 player1 = True
-moves = 0
+turn = False
 winner = ""
 playing = True
+filled = False
 
-for i in range(3):
-    match_field.append([])
-    for j in range(3):
-        match_field[i].append("🔳")
-
-while True:
+while playing:
     for row in match_field:
         for column in row:
             print(column, end=" ")
         print()
-
-    if playing:
+    if not filled:
         if player1:
             user_input = input("Player 1 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
             row = int(user_input[0])
@@ -27,7 +22,6 @@ while True:
                 column -= 1
                 match_field[row][column] = "❌"
                 player1 = not player1
-                playing = False if "🔳" not in match_field else True
             else:
                 print("Introduzca una coordenada valida")
         else:
@@ -39,32 +33,54 @@ while True:
                 column -= 1
                 match_field[row][column] = "🟢"
                 player1 = not player1
-                playing = False if "🔳" not in match_field else True
             else:
                 print("Introduzca una coordenada valida")
     for row in match_field:
-        if row[0] == row[1] and row[1] == row[2]:
-            if row[0] == "❌":
+        if row[0] == row[1] and row[1] == row[2]:                                                          # Horizontal
+            if "🔳" != row[0] == "❌":
                 winner = "player1"
-            else:
+                playing = False
+            elif "🔳" != row[0] == "🟢":
                 winner = "player2"
+                playing = False
+            break
         for column in row:
-            if row[column] == row[column] == row[column]:
-                if row[column] == "":
+            index = row.index(column)
+            if match_field[0][index] == match_field[1][index] == match_field[2][index]:                    # Vertical
+                if "🔳" != match_field[0][index] == "❌":
                     winner = "player1"
-                else:
+                    playing = False
+                elif "🔳" != match_field[0][index] == "🟢":
                     winner = "player2"
+                    playing = False
+                break
     else:
-        if match_field[0][0] == match_field[1][1] and match_field[1][1] == match_field[2][2]:
-            if row[0] == "❌":
-                winner = "player1"
-            else:
-                winner = "player2"
-        if match_field[2][0] == match_field[1][1] and match_field[1][1] == match_field[0][2]:
-            if row[0] == "❌":
-                winner = "player1"
-            else:
-                winner = "player2"
-        break
-    
+        if match_field[0][0] == match_field[1][1] and match_field[1][1] == match_field[2][2]:              # Diagonal
+                if "🔳" != row[index] == "❌":
+                    winner = "player1"
+                    playing = False
+                elif "🔳" != row[index] == "🟢":
+                    winner = "player2"
+                    playing = False
+                break
+        if match_field[2][0] == match_field[1][1] and match_field[1][1] == match_field[0][2]:            # Inversed diagonal
+                if "🔳" != row[index] == "❌":
+                    winner = "player1"
+                    playing = False
+                elif "🔳" != row[index] == "🟢":
+                    winner = "player2"
+                    playing = False
+                break
+        for i in match_field:
+            for j in i:
+                if j == '🔳':
+                    filled = False
+                    break
+        if filled == True:
+            winner = "None"
+
+for row in match_field:
+    for column in row:
+        print(column, end=" ")
+    print()
 print(f"And the winner is: {winner}")
