861ef72c46d4f673260d09b25f5c56f84f8a30a8
ElPayo <elpayov2@gmail.com>
2022-11-17 17:38:56 +0000

    Avances

--- 3_in_a_row.py
+++ 3_in_a_row.py
@@ -3,58 +3,92 @@
 match_field = []
 playing = True
 player1 = True
-moves = 0
 
 for i in range(3):
     match_field.append([])
     for j in range(3):
         match_field[i].append("🔳")
 
-match match_field:
-    case match_field if match_field[0][0] == match_field[1][1] and match_field[1][1] == match_field[2][2]:
-        if match_field[0][0] == "❌":
-            winner = "player1"
-        else:
-            winner = "player2"
-    case match_field if match_field[0][0] == match_field[1][0] and match_field[1][0] == match_field[2][0]:
-        if match_field[0][0] == "❌":
-            winner = "player1"
-        else:
-            winner = "player2"
-    case match_field if match_field[0][0] == match_field[0][1] and match_field[0][1] == match_field[0][2]:
-        if match_field[0][0] == "❌":
-            winner = "player1"
-        else:
-            winner = "player2"
-    case _:
-        print("No one is the winner")
-
-while moves < 9:
+while True:
     for row in match_field:
         for column in row:
             print(column, end=" ")
         print()
-    if player1:
-        user_input = input("Player 1 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
-        row = int(user_input[0])
-        column = int(user_input[1])
-        if row and column <= 3 and match_field[row - 1][column - 1] == "🔳":
-            row -= 1
-            column -= 1
-            match_field[row][column] = "❌"
-            player1 = not player1
-            moves += 1
+    if playing:
+        if player1:
+            user_input = input("Player 1 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
+            row = int(user_input[0])
+            column = int(user_input[1])
+            if row and column <= 3 and match_field[row - 1][column - 1] == "🔳":
+                row -= 1
+                column -= 1
+                match_field[row][column] = "❌"
+                player1 = not player1
+                for box in match_field:
+                    if "🔳" not in box:
+                        playing = False
+                        break
+            else:
+                print("Introduzca una coordenada valida")
         else:
-            print("Introduzca una coordenada valida")
+            user_input = input("Player 2 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
+            row = int(user_input[0])
+            column = int(user_input[1])
+            if row and column <= 3 and match_field[row - 1][column - 1] == "🔳":
+                row -= 1
+                column -= 1
+                match_field[row][column] = "🟢"
+                player1 = not player1
+                for box in match_field:
+                    if "🔳" not in box:
+                        playing = False
+                        break
+            else:
+                print("Introduzca una coordenada valida")
     else:
-        user_input = input("Player 2 introduce the coordenates of which box you'd want your mark to be drawn, separated by a coma: ").split(",")
-        row = int(user_input[0])
-        column = int(user_input[1])
-        if row and column <= 3 and match_field[row - 1][column - 1] == "🔳":
-            row -= 1
-            column -= 1
-            match_field[row][column] = "🟢"
-            player1 = not player1
-            moves += 1
+        break
+
+match match_field:
+    case match_field if match_field[0][0] == match_field[1][1] and match_field[1][1] == match_field[2][2]:    # Diagonal
+        if match_field[0][0] == "❌":
+            winner = "player1"
         else:
-            print("Introduzca una coordenada valida")
+            winner = "player2"
+    case match_field if match_field[0][2] == match_field[1][1] and match_field[1][1] == match_field[2][0]:    # Inversed Diagonal
+        if match_field[0][2] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[0][0] == match_field[1][0] and match_field[1][0] == match_field[2][0]:    # Horizontal 1
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[1][0] == match_field[1][1] and match_field[1][1] == match_field[1][2]:    # Horizontal 2
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[2][0] == match_field[2][1] and match_field[2][1] == match_field[2][2]:    # Horizontal 3
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[0][0] == match_field[1][0] and match_field[1][0] == match_field[2][0]:    # Vertical 1
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[0][1] == match_field[1][1] and match_field[1][1] == match_field[2][1]:    # Vertical 2
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case match_field if match_field[0][2] == match_field[1][2] and match_field[1][2] == match_field[2][2]:    # Vertical 3
+        if match_field[0][0] == "❌":
+            winner = "player1"
+        else:
+            winner = "player2"
+    case _:                                                                                                   # Tie
+        winner = "None"
+print(f"Winner = {winner}")
