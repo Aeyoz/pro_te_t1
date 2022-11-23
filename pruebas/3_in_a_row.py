3351512917073fefa9136b25cea11fc527854dff
ElPayo <elpayov2@gmail.com>
2022-11-23 11:39:22 +0000

    Cambio de nombre de user_input por play

--- pruebas/3_in_a_row.py
+++ pruebas/3_in_a_row.py
@@ -11,9 +11,9 @@ while playing:
         for column in row:
             print(column, end=" ")
         print()
-    user_input = input(f"{player} choice (Ej: 3,1): ").split(",")
-    row = int(user_input[0])
-    column = int(user_input[1])
+    play = input(f"{player} choice (Ej: 3,1): ").split(",")
+    row = int(play[0])
+    column = int(play[1])
     if 1 <= row <= 3 and 1 <= column <= 3 and board[row - 1][column - 1] == "🔳":
         row -= 1; column -= 1; moves += 1
         if player == "Player 1":
